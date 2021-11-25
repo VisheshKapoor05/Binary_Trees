70c3260e9d960c4ffd58b29c04f81041517663db
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-11-25 12:22:15 +0530

    UC1: Added Binary Search Tree and implemented compareTo method

---
+++ BinaryTreeDemo.java
@@ -0,0 +1,16 @@
+package HashMaps_Trees;
+
+public class BinaryTreeDemo {
+	
+	public static void main(String[] args) {
+		BinarySearchTree newTree = new BinarySearchTree();
+		Node root = newTree.insertElement(null, 56);
+		Node node2 = newTree.insertElement(root, 30);
+		Node node3 = newTree.insertElement(node2, 70);
+		
+		
+		//System.out.println(root.data);
+		newTree.printInorder();
+		//newTree.inorderTraversal(root);
+	}
+}
